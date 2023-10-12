cc6df91b0ae7e6491310a4438da5470dc809f623
k6zma <k6zma@yandex.ru>
2023-10-12 19:20:17 +0300

    reffubed atribute name

--- src/ml/binary_relation/data_preprocessing.py
+++ src/ml/binary_relation/data_preprocessing.py
@@ -2,21 +2,21 @@ import pandas as pd
 
 class RelationDataPreprocessing:
 
-    def __init__(self, dataframe: pd.DataFrame):
-        self.dataframe = dataframe
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
 
     def remove_rows_with_nan(self):
-        self.dataframe.dropna(inplace=True)
+        self.df.dropna(inplace=True)
 
     def compute_power_of_set(self):
-        self.dataframe['Power Of Set'] = self.dataframe['M'].apply(lambda x: len(x.split(',')))
+        self.df['Power Of Set'] = self.df['M'].apply(lambda x: len(x.split(',')))
 
     def compute_power_of_relation(self):
-        self.dataframe['Power Of Relation'] = self.dataframe['R'].apply(lambda x: len([i.strip() for i in x.split('),') if i.strip()]))
+        self.df['Power Of Relation'] = self.df['R'].apply(lambda x: len([i.strip() for i in x.split('),') if i.strip()]))
 
     def compute_relation_dimension(self, relation_type: str):
         if relation_type == "Reflexive":
-            self.dataframe['Relation Dimension'] = self.dataframe['R'].apply(self._get_dimension_reflexive)
+            self.df['Relation Dimension'] = self.df['R'].apply(self._get_dimension_reflexive)
 
         elif relation_type not in ["Symmetric", "Transitive"]:
             print("ERROR!!! Unidentified property of a binary relation!")
@@ -31,7 +31,7 @@ class RelationDataPreprocessing:
 
     def count_symmetric_pairs(self, relation_type: str):
         if relation_type == "Symmetric":
-            self.dataframe['Symmetric Pairs Count'] = self.dataframe['R'].apply(self._get_symmetric_count)
+            self.df['Symmetric Pairs Count'] = self.df['R'].apply(self._get_symmetric_count)
         elif relation_type not in ["Reflexive", "Transitive"]:
             print("ERROR!!! Unidentified property of a binary relation!")
 
@@ -50,7 +50,7 @@ class RelationDataPreprocessing:
 
     def count_transitive_pairs(self, relation_type: str):
         if relation_type == "Transitive":
-            self.dataframe['Transitive Triples Count'] = self.dataframe['R'].apply(self._get_transitive_count)
+            self.df['Transitive Triples Count'] = self.df['R'].apply(self._get_transitive_count)
         elif relation_type not in ["Reflexive", "Symmetric"]:
             print("ERROR!!! Unidentified property of a binary relation!")
 
@@ -67,8 +67,8 @@ class RelationDataPreprocessing:
             return 0
 
     def move_type_to_end(self):
-        cols = [col for col in self.dataframe.columns if col != 'Type'] + ['Type']
-        self.dataframe = self.dataframe[cols]
+        cols = [col for col in self.df.columns if col != 'Type'] + ['Type']
+        self.df = self.df[cols]
 
     def get_dataframe(self) -> pd.DataFrame:
-        return self.dataframe
+        return self.df
