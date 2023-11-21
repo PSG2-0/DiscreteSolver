bfc274f60e61f35f679ebb1e3f649b4573997b7d
k6zma <87338187+k6zma@users.noreply.github.com>
2023-11-21 10:48:15 +0300

    fixed bug with symbols detection

--- src/math_algos/set_theory/set_simplifier.py
+++ src/math_algos/set_theory/set_simplifier.py
@@ -9,7 +9,7 @@ class SetSimplifier:
             '∩': '&',
             '∪': '|',
             '\\': '& ~',
-            'not(': '~(',
+            'not ': '~',
             '∅': 'False',
             'U': 'True',
             '∆': '^'
@@ -25,13 +25,18 @@ class SetSimplifier:
         }
 
     def transform(self, input_expr):
+        input_expr = input_expr.replace('not(', '~(')
         for old, new in self.replacements.items():
             input_expr = input_expr.replace(old, new)
         return input_expr
 
     def simplify_expression(self, expr_str):
         expr_str = self.transform(expr_str)
-        expr = parse_expr(expr_str, transformations=self.transformations)
+
+        all_symbols = set(re.findall(r'\b[A-Za-z]+\b', expr_str))
+        symbols_dict = {s: symbols(s) for s in all_symbols}
+        
+        expr = parse_expr(expr_str, local_dict=symbols_dict, transformations=self.transformations)
         simplified_expr = simplify_logic(expr, form='dnf', force=True)
         return simplified_expr
 
@@ -40,5 +45,4 @@ class SetSimplifier:
         for old, new in self.reverse_replacements.items():
             reversed_str = reversed_str.replace(old, new)
         reversed_str = re.sub(r'~\(?([A-Za-z0-9_]+)\)?', r'not(\1)', reversed_str)
-        reversed_str = re.sub(r'not ([A-Za-z0-9_]+)', r'not(\1)', reversed_str)
         return reversed_str
